9ded1cee7f31c3af7596117cb0efe3542f5a2e01
Ilia Donskikh <donivrecord@gmail.com>
2025-07-26 18:23:02 +0300

    Add user input model

--- src/types/models/core.ts
+++ src/types/models/core.ts
@@ -1,3 +1,4 @@
 export * from "./core/message";
 export * from "./core/userProvider";
 export * from "./core/channel";
+export * from "./core/userInputRequest";
---
+++ src/types/models/core/userInputRequest.ts
@@ -0,0 +1,11 @@
+export enum CoreUserInputRequestStatus {
+  created = "created",
+  completed = "completed",
+}
+
+export enum CoreUserInputRequestPayloadRouterCode {
+  systemAction = "systemAction",
+  systemKnowledge = "systemKnowledge",
+  topicKnowledge = "topicKnowledge",
+  commonKnowledge = "commonKnowledge",
+}
